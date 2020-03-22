bdf990962b645ad435f76803c7b1c84e83366e0b
admin <admin@MacBook-Pro-admin.local>
2020-03-22 14:58:57 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// You should implement your task here.
+
+module.exports = function towelSort (matrix) {
+    if (!arguments.length) return [];
+    let mas = [];
+    matrix.forEach((item, i) => {
+        if (!(i % 2))
+            Array.prototype.push.apply(mas, item);
+        else
+            Array.prototype.push.apply(mas, item.reverse());
+    });
+  return mas;
+}
